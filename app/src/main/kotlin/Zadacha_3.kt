7a0fc5223143988bfd6d29304b4be4d7d7a16b5e
stas_ <finist13433@gmail.ru>
2026-01-22 20:46:18 +0700

    KS-1-3 zadacha №3

---
+++ app/src/main/kotlin/Zadacha_3.kt
@@ -0,0 +1,21 @@
+package org.example.app
+
+fun main() {
+
+    val year: Int = 1961
+    var hour: String = "09"
+    var minute: String = "07"
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    print(hour+":")
+    println(minute)
+
+    //println(hour +":"+ minute)
+    //println("${hour}:${minute}")
+}
