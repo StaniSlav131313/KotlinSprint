6ff8c7392a72c30470620a366f3747c9dd456149
stas_ <finist13433@gmail.ru>
2026-01-22 20:09:22 +0700

    KS-1-1 zadacha1

---
+++ app/src/main/kotlin/Lession_1/1-1-variables.kt
@@ -0,0 +1,11 @@
+package org.example.app.Lession_1
+
+fun main(){
+
+    var numberOfOrder: Int = 72
+    val thanksMessage: String = "Thank you for purchasing this product!"
+
+    println(numberOfOrder)
+    println(thanksMessage)
+
+}
