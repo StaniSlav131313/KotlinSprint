93fa7860d36aa479fcc004f6d80f189c866d33bf
stas_ <finist13433@gmail.ru>
2026-01-22 19:25:25 +0700

    Les-1-1

---
+++ app/src/main/kotlin/Lession_1/Zadacha_1.kt
@@ -0,0 +1,10 @@
+package org.example.app.Lession_1
+
+fun main(){
+
+    var numberOfOrders: Int = 72
+    val thanksMessage: String = "Thank you for purchasing this product!"
+
+    println(numberOfOrders)
+    println(thanksMessage)
+}
