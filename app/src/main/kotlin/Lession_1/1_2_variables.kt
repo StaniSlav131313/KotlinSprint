420250a55986c61e20da97a77e0d6c25010ea446
stas_ <finist13433@gmail.ru>
2026-01-22 20:26:59 +0700

    KS-1-2 zadacha №2

---
+++ app/src/main/kotlin/Lession_1/1_2_variables.kt
@@ -0,0 +1,20 @@
+package org.example.app.Lession_1
+
+fun main() {
+
+    var numberOfOrder: Int = 72
+    val thanksMessage: String = "Thank you for purchasing this product!"
+
+    println(numberOfOrder)
+    println(thanksMessage)
+
+    var numberOfEmployees: Int = 2000
+
+    //println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+
+    println(numberOfEmployees)
+
+
+}
