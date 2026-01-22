9607d114a0c138510837c7125d215e9449af5ff6
stas_ <finist13433@gmail.ru>
2026-01-22 21:02:49 +0700

    KS-1-4 zadacha №4

---
+++ app/src/main/kotlin/Zadanie_4.kt
@@ -0,0 +1,19 @@
+package org.example.app
+
+fun main() {
+
+    val length: Long = 4086860000
+    val age: Byte = 27
+    val partOfDay: Double = 0.075
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 32700
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+
+}
