f2989fe03c075b669a271777c7b6e922a7569c6b
stas_ <finist13433@gmail.ru>
2026-01-23 19:04:41 +0700

    KS-1-4 zadacha №4 (Correction)

--- app/src/main/kotlin/Zadanie_4.kt
+++ app/src/main/kotlin/Zadanie_4.kt
@@ -2,18 +2,18 @@ package org.example.app
 
 fun main() {
 
-    val length: Long = 4086860000
+    val length: Long = 40868600000
     val age: Byte = 27
-    val partOfDay: Double = 0.075
+    val partOfDay: Float = 0.075f
     val seconds: Short = 6480
     val partOfYear: Double = 2.0547945205479453E-4
-    val apogee: Int = 32700
+    val apogee: Int = 327000
 
-    println(length)
-    println(age)
-    println(partOfDay)
-    println(seconds)
-    println(partOfYear)
-    println(apogee)
+    println("Distance(mm): $length")
+    println("Age of the astronaut: $age")
+    println("Part of the day spent in space: $partOfDay")
+    println("Number of seconds spent in space: $seconds")
+    println("Part of the year spent in space: $partOfYear")
+    println("Orbital apogee, meters: $apogee")
 
 }
