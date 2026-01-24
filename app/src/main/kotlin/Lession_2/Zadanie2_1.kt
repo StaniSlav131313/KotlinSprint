d65060af19ca1429daee670f1741535ec6840eca
stas_ <finist13433@gmail.ru>
2026-01-24 17:34:39 +0700

    KS-2-1 zadacha №1

---
+++ app/src/main/kotlin/Lession_2/Zadanie2_1.kt
@@ -0,0 +1,16 @@
+package org.example.app.Lession_2
+
+fun main() {
+
+    val englishClassStudents = 4
+
+    val firstStudent = 3
+    val secondStudent = 4
+    val thirdStudent = 3
+    val fourthStudent = 5
+
+    val classRatingAverage = (firstStudent + secondStudent + thirdStudent + fourthStudent) / englishClassStudents.toFloat()
+
+    println(classRatingAverage)
+
+}
