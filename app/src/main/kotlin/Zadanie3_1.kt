691575aace32e8d14f8b4e7ae0fed0879941cfda
stas_ <finist13433@gmail.ru>
2026-01-26 18:01:04 +0700

    KS-3-1 zadacha №1

---
+++ app/src/main/kotlin/Zadanie3_1.kt
@@ -0,0 +1,14 @@
+package org.example.app
+
+fun main() {
+
+    var userName = "Alice"
+    var hello = "Good morning"
+
+    println("$hello, $userName!")
+    
+    hello = "Good evening"
+
+    println("$hello, $userName!")
+
+}
